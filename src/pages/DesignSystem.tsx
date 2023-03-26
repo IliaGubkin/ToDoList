b751068a32637105c829a14ad6576fee4ba1f21a
IliaGubkin <101293525+IliaGubkin@users.noreply.github.com>
2023-03-26 14:38:18 +0300

    INP-45 MadButton переход на внешний сайт

--- src/pages/DesignSystem.tsx
+++ src/pages/DesignSystem.tsx
@@ -1,14 +1,17 @@
-import { useNavigate } from "react-router-dom";
+import { Route, useNavigate } from "react-router-dom";
 import { MadButton } from "../components/MadButton/MadButton";
 // 
 export function DesignSystem() {
     const navigate = useNavigate()
-
-    return(
+    const handleClick = () => {
+        window.location.replace('https://google.com/');
+    };
+    return (
         <>
-            <MadButton size="lg" importance={"primary"} icon={"arrowDown"} func={() => console.log("lul")}/>
-            <MadButton size="md" importance={"secondary"} icon={"arrowUp"} func={() => navigate("/slider")}/>
-            <MadButton size="sm" importance={"active"} icon={"none"} func={() => alert("я попап окно")}/>
+            <MadButton size="lg" importance={"primary"} icon={"arrowDown"} func={() => console.log("lul")} />
+            <MadButton size="md" importance={"secondary"} icon={"arrowUp"} func={() => navigate("/slider")} />
+            <MadButton size="sm" importance={"primary"} icon={"arrowDown"} func={() => handleClick()} />
+            <MadButton size="sm" importance={"active"} icon={"none"} func={() => alert("я попап окно")} />
         </>
     )
 }
