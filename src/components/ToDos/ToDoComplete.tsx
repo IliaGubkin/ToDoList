5d95fcd70d65c314163a8b085274f50e2227462e
IliaGubkin <101293525+IliaGubkin@users.noreply.github.com>
2023-04-05 23:50:14 +0300

    fix completed

--- src/components/ToDos/ToDoComplete.tsx
+++ src/components/ToDos/ToDoComplete.tsx
@@ -1,25 +1,32 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { setCompleted } from "../../store/toDoItem/actions";
 import { IToDo, IToDoComplete } from "./Types";
 
-export function ToDoComplete({toDoList}: IToDoComplete) {
+export function ToDoComplete({ toDoList }: IToDoComplete) {
+  const state = useSelector((state) => state);
+  const completed = state.toDoItem.completed
+  const dispatch = useDispatch();
+  const [toDo, setTodo] = useState({})
+
   function toDoComplete(item: IToDo) {
-    // сделать новое состояние с массивом либо setTimeout
-    const dispatch = useDispatch();
-    const state = useSelector((state) => state);
-    const completed = state.toDoItem.completed
-    dispatch(setCompleted(!completed));
-    toDoList.forEach((elem: IToDo) => {
-      if (item.id == elem.id) {
-        (elem.completed = completed);
-      }
-    });
+    dispatch(setCompleted(!item.completed));
+    setTodo(item);
   }
-//
+
+  toDoList.forEach((elem: IToDo) => {
+    if (toDo.id == elem.id) {
+      (elem.completed = completed);
+    }
+  })
+
   return (
     <div className="todo-author">
       {
         toDoList.map((item: IToDo) => (
-          <div className="todo-author__button" onClick={() => toDoComplete(item)} key={item.id}>{item.completed ? "☑" : "☐"}</div>
+          <div className="todo-author__button" onClick={() => {
+            toDoComplete(item);
+          }} key={item.id}>{item.completed ? "☑" : "☐"}</div>
         ))
       }
     </div>
